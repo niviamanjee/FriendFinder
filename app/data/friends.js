293873576e83b7a80e525cb0669f63cec5c9ec0b
Nivi Amanjee <nivedita.amanjee@gmail.com>
2019-12-16 15:19:55 -0800

    created an empty array for the users in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,6 @@
+var friendsArray = [];
+//this sets the array of FriendFinder users
+//which will be created dynamically as users sign into the app
+
+module.exports = friendsArray;
+//makes the array accessible to other files using require
