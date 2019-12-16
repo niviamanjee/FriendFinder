0bd093995c0579d07470f7df2f744247835d1b98
Nivi Amanjee <nivedita.amanjee@gmail.com>
2019-12-16 14:55:04 -0800

    created server route to html pages

---
+++ server.js
@@ -0,0 +1,27 @@
+/***************************************
+MODULES, PACKAGES
+***************************************/
+// CORE MODULES
+const path = require('path');
+
+// NPM PACKAGES
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// listens either on PORT set by deployment or local PORT 3000
+const PORT = process.env.PORT || 3000;
+
+// parse application
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/assets'));
+
+// CUSTOM MODULES
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function () {
+    console.log("Server has started on PORT 3000.");
+});
